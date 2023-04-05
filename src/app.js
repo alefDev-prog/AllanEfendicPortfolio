5a6077126aa0c95309f047e40d5d418f540d4b02
alefCS50 <allan.efendic@icloud.com>
2023-04-05 09:48:05 +0200

    feat: proj-pictures

--- src/app.js
+++ src/app.js
@@ -150,7 +150,6 @@ const display = async () => {
         const project = document.createElement('div');
         project.classList.add('card');
         container.appendChild(project);
-        
     })
     
 }
