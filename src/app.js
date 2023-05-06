162e93f41d08db53f7a707d6b1b1fcb96916198e
alefCS50 <allan.efendic@icloud.com>
2023-05-06 19:38:11 +0200

    fix responsiveness

--- src/app.js
+++ src/app.js
@@ -124,7 +124,7 @@ const projectsArr = [];
 //async stuff
 const getAll = async () => {
     
-    const response = await fetch('https://alefdev-prog.github.io/AllanEfendicPortfolio.github.io/projects.json', {
+    const response = await fetch('projects.json', {
         method:'GET',
         headers: {
             'content-type': 'application/json'
